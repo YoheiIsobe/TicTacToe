be67a2c0cfac54430e96d89f5b7a40663006847b
YoheiIsobe <takuma0513n@gmail.com>
2025-11-22 22:45:09 +0900

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Takuma Nezu on 2025/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
